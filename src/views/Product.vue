7e0d7f0d5977398bf8b7eb90021e9aa38eadafcb
Niek <17721120+Niekvan@users.noreply.github.com>
2021-01-04 21:09:55 +0000

    Add ProductDetail and update navigation

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>Product</h1>
+  <Navigation>
+    <template v-slot:default>
+      Detail
+    </template>
+    <template v-slot:right>
+      <router-link
+        to="/"
+        class="py-4 px-6 mr-2 border bg-white text-xl capitalize hover:bg-gray-100"
+      >
+        Go Back
+      </router-link>
+      <router-link
+        to="/result"
+        class="py-4 px-6 border bg-white text-xl capitalize hover:bg-gray-100"
+      >
+        Finish
+      </router-link>
+    </template>
+  </Navigation>
+  <ProductDetail />
 </template>
 
 <script>
+import Navigation from '@/components/Navigation';
+import ProductDetail from '@/components/ProductDetail';
+
 export default {
-  name: 'Product'
+  name: 'Product',
+  components: {
+    Navigation,
+    ProductDetail
+  }
 };
 </script>
--- tests/e2e/specs/product.js
+++ tests/e2e/specs/product.js
@@ -1,22 +1,24 @@
 describe('display single product', () => {
-  const jacket = 'Mens Cotton Jacket';
+  it('should show the product details', () => {
+    const jacket = 'Mens Cotton Jacket';
 
-  cy.server({ force404: true }); // Force 404 response if the call is not mocked in Cypress
+    cy.server({ force404: true }); // Force 404 response if the call is not mocked in Cypress
 
-  cy.route({
-    method: 'GET',
-    url: 'https://fakestoreapi.com/products/(1|3)',
-    response: {
-      id: 3,
-      title: jacket,
-      price: 55.99,
-      description:
-        'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
-      category: 'men clothing',
-      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg'
-    }
+    cy.route({
+      method: 'GET',
+      url: 'https://fakestoreapi.com/products/(1|3)',
+      response: {
+        id: 3,
+        title: jacket,
+        price: 55.99,
+        description:
+          'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
+        category: 'men clothing',
+        image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg'
+      }
+    });
+
+    cy.visit('/product/1');
+    cy.contains(`Details ${jacket}`);
   });
-
-  cy.visit('/product/1');
-  cy.contains(`Details ${jacket}`);
 });
