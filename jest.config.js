ca3ad24ac59ced5c353016a661f4c794b722e64a
Niek <17721120+Niekvan@users.noreply.github.com>
2021-01-04 20:38:57 +0000

    Navigation bar

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  preset: '@vue/cli-plugin-unit-jest',
   transform: {
-    "^.+\\.vue$": "vue-jest"
+    '^.+\\.vue$': 'vue-jest'
   }
 };
--- src/components/ProductPreview.vue
+++ src/components/ProductPreview.vue
@@ -1,16 +1,16 @@
 <template>
   <router-link
     :to="`/product/${product.id}`"
-    class="mx-5 p-10 bg-white text-center text-xs uppercase border"
+    class="m-5 w-52 bg-white text-center text-xs uppercase hover:underline"
   >
-    <div class="w-56 h-56">
+    <div class="w-52 h-52">
       <img
         class="object-contain h-full w-full transform transition-transform duration-300 hover:scale-110"
         :src="product.image"
         :alt="product.description"
       />
     </div>
-    <p data-test="product" class="w-48 mx-4 mt-8">{{ product.title }}</p>
+    <p data-test="product" class="w-full px-4 mt-8">{{ product.title }}</p>
   </router-link>
 </template>
 
